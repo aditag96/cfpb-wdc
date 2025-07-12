3825d98a773b7241194d26e9b68cad7c71f55964
aditag96 <aditag96@gmail.com>
2025-07-11 20:02:49 -0500

    Remove markdown fences & add tableau.init()

--- connector.js
+++ connector.js
@@ -1,4 +1,3 @@
-```js
 (function() {
   var myConnector = tableau.makeConnector();
 
@@ -8,58 +7,62 @@
                  '&date_received_max=' + end +
                  '&size=' + pageSize +
                  '&page=' + (pageNum-1);
-    return fetch(url + params).then(function(resp) {
-      if (!resp.ok) throw new Error('HTTP ' + resp.status);
-      return resp.json();
-    });
+    return fetch(url + params)
+      .then(function(resp) {
+        if (!resp.ok) throw new Error('HTTP ' + resp.status);
+        return resp.json();
+      });
   }
 
   myConnector.getSchema = function(schemaCallback) {
-    var [start, end] = tableau.connectionData.split(',');
-    fetchAPI(start, end, 1, 1)
-      .then(json => {
-        var first = json.hits.hits[0]._source;
-        var cols = Object.keys(first).map(key => ({
-          id: key,
-          alias: key,
-          dataType: typeof first[key] === 'number'
-            ? tableau.dataTypeEnum.int
-            : /^\d{4}-\d{2}-\d{2}/.test(first[key])
-              ? tableau.dataTypeEnum.date
-              : tableau.dataTypeEnum.string
-        }));
-        schemaCallback([{ id: 'cfpbComplaints', alias: 'CFPB Complaints', columns: cols }]);
-      })
-      .catch(err => tableau.abortWithError(err.message));
+    var parts = tableau.connectionData.split(',');
+    fetchAPI(parts[0], parts[1], 1, 1).then(function(json) {
+      var first = json.hits.hits[0]._source;
+      var cols = Object.keys(first).map(function(key) {
+        var val = first[key];
+        var dt = tableau.dataTypeEnum.string;
+        if (typeof val === 'number') dt = tableau.dataTypeEnum.int;
+        else if (/^\d{4}-\d{2}-\d{2}/.test(val)) dt = tableau.dataTypeEnum.date;
+        return { id: key, alias: key, dataType: dt };
+      });
+      schemaCallback([{
+        id: 'cfpbComplaints',
+        alias: 'CFPB Complaints',
+        columns: cols
+      }]);
+    }).catch(function(err) {
+      tableau.abortWithError(err.message);
+    });
   };
 
   myConnector.getData = function(table, doneCallback) {
-    var [start, end] = tableau.connectionData.split(',');
+    var parts = tableau.connectionData.split(',');
     var pageSize = 10000, pageNum = 1;
-    function iterate() {
-      fetchAPI(start, end, pageNum++, pageSize)
-        .then(json => {
+    (function iterate() {
+      fetchAPI(parts[0], parts[1], pageNum++, pageSize)
+        .then(function(json) {
           var hits = json.hits.hits;
           if (!hits.length) return doneCallback();
           table.appendRows(hits.map(r => r._source));
           iterate();
         })
-        .catch(err => tableau.abortWithError(err.message));
-    }
-    iterate();
+        .catch(function(err) {
+          tableau.abortWithError(err.message);
+        });
+    })();
   };
 
+  // Only register *after* tableau.init() has been called in index.html
   tableau.registerConnector(myConnector);
-  document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('form').addEventListener('submit', evt => {
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form').addEventListener('submit', function(evt) {
       evt.preventDefault();
-      tableau.connectionData = [
-        document.getElementById('start_date').value,
-        document.getElementById('end_date').value
-      ].join(',');
-      tableau.connectionName = 'CFPB Complaints Full API';
+      var start = document.getElementById('start_date').value;
+      var end   = document.getElementById('end_date').value;
+      tableau.connectionData   = start + ',' + end;
+      tableau.connectionName   = 'CFPB Complaints Full API';
       tableau.submit();
     });
   });
 })();
-```
